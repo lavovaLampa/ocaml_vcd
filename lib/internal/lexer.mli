15bd819f1cb199c2ac7c1d1ba350d380becac6db
Martin Cizmar (LavovaLampa) <betacheer@gmail.com>
2023-12-31 23:27:57 +0100

    Fix formatting

--- lib/internal/lexer.mli
+++ lib/internal/lexer.mli
@@ -1,5 +1,6 @@
 type binary_value = Zero | One | X | Z | U | Nothing [@@deriving show]
 
+(** VCD declaration section token*)
 type declaration_token =
   | Comment
   | Date
@@ -10,8 +11,8 @@ type declaration_token =
   | Var
   | Version
 [@@deriving show]
-(** VCD declaration section token*)
 
+(** VCD simulation section token *)
 type simulation_token =
   | Comment
   | DumpAll
@@ -23,15 +24,14 @@ type simulation_token =
   | BinaryVector
   | RealVector
 [@@deriving show]
-(** VCD simulation section token *)
 
+(** value change token (use in $dump* statements) *)
 type value_change_token =
   | Value of binary_value
   | BinaryVector
   | RealVector
   | End
 [@@deriving show]
-(** value change token (use in $dump* statements) *)
 
 type reference =
   | Scalar of string
@@ -45,20 +45,23 @@ type _ token_type =
   | Reference : reference token_type
   | Number : int token_type
   | BinaryNumber : binary_value array token_type
-  | AnyText : string token_type
-(** Lexer token type (used in pull mode) *)
+  | AnyText : string token_type  (** Lexer token type (used in pull mode) *)
 
 exception EndOfFile
 
 val show_token_type : 't. 't token_type -> string
 (** [token_type] pretty-printer *)
+
 val lex_identifier : 't. Sedlexing.lexbuf -> 't token_type -> 't option
 (** [lex_identifier lexbuf token_type] returns if given [token_type] can be lexed,
     else returns [None] *)
+
 val lex_declaration : Sedlexing.lexbuf -> declaration_token option
 (** [lex_declaration lexbuf] returns tokens during declaration phase *)
+
 val lex_simulation : Sedlexing.lexbuf -> simulation_token option
 (** [lex_simulation lexbuf] returns tokens during simulation phase *)
+
 val lex_value_change : Sedlexing.lexbuf -> value_change_token option
 (** [lex_value_change lexbuf] returns tokens while lexing inside 
     $dumpon/$dumpoff/$dumpall/$dumpvars statement *)
